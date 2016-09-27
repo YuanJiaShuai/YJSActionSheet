726a93139882b6b658ebbab13f77d81331ea78b7
yjs_maoge@163.com <apple@appledemacbook-air.local>
2016-09-27 14:39:00 +0800

    Initial Commit

---
+++ 自定义弹出界面/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义弹出界面
+//
+//  Created by apple on 16/9/27.
+//  Copyright © 2016年 YJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义弹出界面/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义弹出界面
+//
+//  Created by apple on 16/9/27.
+//  Copyright © 2016年 YJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
